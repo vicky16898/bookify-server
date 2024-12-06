8c2a72617fa8c3038ab18d781bf77ae6cbe6d541
Vicky Daniel <vicky16898@gmail.com>
2024-12-06 14:25:39 -0500

    fix front end url

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL
+    origin: [
+        'http://localhost:3000',  // Local development
+        'https://ezbookify.netlify.app',  // Production frontend URL
+        // Add any other origins you want to allow
+      ]
+    
 }));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "bookify",
